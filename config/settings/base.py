4d386bfa70fde4a84bdf19440fe3caefa5788e66
unknown <seyyed.mahdi.sepahbodi.1998@gmail.com>
2026-01-27 08:16:34 -0800

    Add basic throttling for apis in config/settings/base.py file.

--- config/settings/base.py
+++ config/settings/base.py
@@ -97,6 +97,17 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+REST_FRAMEWORK.update({
+  "DEFAULT_THROTTLE_CLASSES": [
+      "rest_framework.throttling.UserRateThrottle",
+      "rest_framework.throttling.AnonRateThrottle",
+  ],
+  "DEFAULT_THROTTLE_RATES": {
+      "user": "200/hour",
+      "anon": "50/hour",
+  }
+})
+
 SPECTACULAR_SETTINGS = {
     "TITLE": "Task Management API",
     "DESCRIPTION": "Projects and Tasks API with JWT auth",
