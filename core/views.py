7852fce175cf76dd0dea945805e44728982e80d5
unknown <seyyed.mahdi.sepahbodi.1998@gmail.com>
2026-01-27 08:38:40 -0800

    Add project CRUD viewsets in core/views.py file.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from models import Project, Task
+from serializers import ProjectSerializer, TaskSerializer
+from permissions import IsProjectOwnerOrReadOnly
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    permission_classes = [IsProjectOwnerOrReadOnly]
+    search_fields = ["title"]
+
+    def get_queryset(self):
+        return Project.objects.filter(owner=self.request.user).order_by("-created_at")
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
