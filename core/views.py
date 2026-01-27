b24010552bb4b92e922f7e52c09b0cffd681aa2c
unknown <seyyed.mahdi.sepahbodi.1998@gmail.com>
2026-01-27 08:48:48 -0800

    Add task CRUD viewset in core/views.py file.

--- core/views.py
+++ core/views.py
@@ -14,3 +14,27 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    filterset_fields = ["status", "priority", "due_date"]
+    ordering_fields = ["created_at", "due_date", "priority"]
+    search_fields = ["title"]
+
+    def get_project(self):
+        project_id = self.kwargs.get("project_pk")
+        try:
+            project = Project.objects.get(id=project_id)
+        except Project.DoesNotExist:
+            raise PermissionDenied("Project not found.")
+        if project.owner != self.request.user:
+            raise PermissionDenied("You do not have access to this project.")
+        return project
+
+    def get_queryset(self):
+        project = self.get_project()
+        return Task.objects.filter(project=project).order_by("-created_at")
+
+    def perform_create(self, serializer):
+        project = self.get_project()
+        serializer.save(project=project)
